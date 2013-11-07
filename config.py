e283e760ae39d4ad08b67fc517df45993db4a4e2
Chad Oliver <hello@chadoliver.co.nz>
2013-11-08 10:48:05 +1300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+INPUT_FOLDER = "./input"
+OUTPUT_ROOT_FOLDER = "./output/"
+
+STYLE_ID = 1
+RESOLUTION = 256
+NUM_WORKER_THREADS = 20
+
+API_KEY = "8ee2a50541944fb9bcedded5165f09d9" # This is straight off the cloudmade documentation pages. Replace it with your own!
